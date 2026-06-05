46052384fd2abb1b97c934ccd485b295824c69bf
vahid sahray <vahidsahray@93gmail.com>
2026-06-05 21:28:28 +0330

    Register course models in admin panel

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Course, Module, Lesson
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_active")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ("title", "course", "order")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("title", "module", "order")
+    prepopulated_fields = {"slug": ("title",)}
